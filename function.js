71ad93a03c0e0f46c45360deb9394443acb36b6b
Chandrashekhar1999 <shekhary1999@gmail.com>
2024-04-27 21:30:56 +0530

    ssave

--- function.js
+++ function.js
@@ -15,3 +15,21 @@ function areaOfRectangle(h, w) {
 }
 console.log(areaOfRectangle(24, 24)); 
 
+// Function to convert Celsius to Fahrenheit
+
+function celciusToFahrenheit(celcTemp) {
+    var fahrenheitTemp = (celcTemp * 9/5) + 32;
+    
+    console.log(`${celcTemp}°C = ${fahrenheitTemp}°F`);
+}
+
+celciusToFahrenheit(25);
+
+
+//Temperature converter fahrenheit to celcius 
+
+function fahrenheitToCelcius (fahrenheitTemp){
+    var celciusTemperature = (fahrenheitTemp - 32) * 5/9 ;
+    console.log(`${fahrenheitTemp}°F  = ${celciusTemperature}°C`);
+}
+fahrenheitToCelcius(77)
--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,12 @@
-function text(name) {
-    console.log("Hello, " + name );
-}
-text("Shekhar");
+console.log ("hello world")
+
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+
+console.log(2+2)
+console.log(2+1)
+console.log(1+2)
+console.log(232+1)
