308d0e73e039449d4bdda973ba055a3b85cde385
Chandrashekhar1999 <shekhary1999@gmail.com>
2024-04-28 10:56:07 +0530

    caluclator.js

---
+++ calculator/calculator.js
@@ -0,0 +1,31 @@
+// Add function
+function add(x, y) {
+    let result = x + y;
+    console.log(result);
+}
+
+add(1, 8);
+
+// Add sub
+function sub(x, y) {
+    let result = x - y;
+    console.log(result);
+}
+
+sub(1, 8);
+
+// Multiply function
+function multiply(x, y) {
+    let result = x * y;
+    console.log(result);
+}
+
+multiply(1, 8);
+
+// divide function
+function divide(x, y) {
+    let result = x / y;
+    console.log(result);
+}
+
+divide(1, 8);
